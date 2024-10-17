a01cf0d3c4123e5d2a457bba0e5b17d199714229
Marciano <italomarcianoaraujo@gmail.com>
2024-10-17 09:21:22 -0300

    Projeto finalizado

---
+++ CarouselView/CarouselTableView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselTableView.swift
+//  ChefDelivery
+//
+//  Created by italo Marciano on 17/10/24.
+//
+
+import SwiftUI
+
+struct CarouselTableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselTableView()
+}
---
+++ StoresView/StoresConteinerView.swift
@@ -0,0 +1,18 @@
+//
+//  StoresConteinerView.swift
+//  ChefDelivery
+//
+//  Created by italo Marciano on 17/10/24.
+//
+
+import SwiftUI
+
+struct StoresConteinerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoresConteinerView()
+}
