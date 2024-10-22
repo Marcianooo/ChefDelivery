e6e9c5e4e709dd51028247d5c9dcd16828d127ce
Marciano <italomarcianoaraujo@gmail.com>
2024-10-22 18:39:38 -0300

    Att

---
+++ Double+.swift
@@ -0,0 +1,8 @@
+//
+//  Double+.swift
+//  ChefDelivery
+//
+//  Created by italo Marciano on 22/10/24.
+//
+
+import Foundation
---
+++ StoresView/StoreDetailView/StoreDetailProductItemView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreDetailProductItemView.swift
+//  ChefDelivery
+//
+//  Created by italo Marciano on 22/10/24.
+//
+
+import SwiftUI
+
+struct StoreDetailProductItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreDetailProductItemView()
+}
